9c4f5f348af5cc70be62fa9315b9ffa9c0186d39
Christian <c@foo.at>
2019-05-24 00:02:02 +0200

    final version

---
+++ Masterarbeit/cleanup.bat
@@ -0,0 +1,50 @@
+del *.aux
+del *.log
+del *.lof
+del *.lol
+del *.synctex.gz
+del *.tdo
+del *.bak
+del *.loa
+del *.log
+del *.lot
+del *.bbl
+del *.blg
+del *.dvi
+del *.out
+del *.brf
+del *.thm
+del *.toc
+del *.idx
+del *.backup
+
+cd tex
+
+del *.aux
+del *.log
+del *.lof
+del *.lol
+del *.synctex.gz
+del *.tdo
+del *.bak
+del *.loa
+del *.log
+del *.lot
+del *.bbl
+del *.blg
+del *.dvi
+del *.out
+del *.brf
+del *.thm
+del *.toc
+del *.idx
+del *.backup
+
+cd append
+
+del *.aux
+
+cd ..
+cd ..
+
+:: see: http://www.mrunix.de/forums/showthread.php?70927-Nach-Compilierung-Dateien-automatisch-l%F6schen
